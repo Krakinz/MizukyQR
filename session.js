a846d8c26faff8607e12fb23950858d0d8a95216
Krakinz <103070588+Krakinz@users.noreply.github.com>
2022-06-18 23:32:28 +0300

    Update session.js

--- session.js
+++ session.js
@@ -1,6 +1,9 @@
-cons fs=require('fs')
-cons chalk=require('chalk')
-cons string=requre(index.js)
-cons os=require('os')
-fs.asyc(./index.js)
-chalk(console.log(os.system(node index.js)))
+cons fs=require('fs');
+cons chalk=require('chalk');
+cons string=requre(index.js);
+cons os=require('os');
+async function String() {
+fs.asyc(index.js);
+chalk.green.bold(os.system(node index.js));
+}
+String{}
