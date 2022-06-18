81f837011da6a141a1af99ba26a3c150e5df7fe2
Krakinz <103070588+Krakinz@users.noreply.github.com>
2022-06-18 23:42:03 +0300

    Update and rename session.js to index.html

--- session.js
+++
@@ -1,9 +0,0 @@
-const fs=require('fs');
-const chalk=require('chalk');
-const string=requre(index.js);
-const os=require('os');
-async function Buffer() {
-fs.asyc(index.js);
-chalk.green.bold(os.system(node index.js));
-}
-Buffer{}
