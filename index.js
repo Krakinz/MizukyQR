820690a9fbd0cf11108084405fae7382ee6694ac
Krakinz <103070588+Krakinz@users.noreply.github.com>
2022-06-18 22:31:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* Copyright (C) 2020 KrakinzLab
 
-Licensed under the  GPL-3.0 License;
+Licensed under the  Apache-2.0 License;
 you may not use this file except in compliance with the License.
 
 KrakinzBot - Krakinzkon
